346fde36e7a0cfbc4f07bc97b9e604828572f48b
Jesse Copeland <jesse.copeland@gmail.com>
2018-11-16 12:08:06 -1000

    Add mock db

---
+++ src/lib/books.db.js
@@ -0,0 +1,37 @@
+const booksFromFakeDB = [{
+  _id : 1,
+  title : 'Talion: The Revenant from DB',
+  author : 'Michael A. Stackpole'
+},
+{
+  _id : 2,
+  title : 'Ready Player One from DB',
+  author : 'Ernest Cline'
+},
+{
+  _id : 3,
+  title : 'Enders Game from DB',
+  author : 'Orson Scott Card'
+}
+];
+let bookIndex = 4;
+
+export const getBooksFromFakeXHR = () => new Promise((resolve, reject) => {
+  setTimeout(() => resolve([...booksFromFakeDB]), 500);
+});
+
+export const addBookToFakeXHR = (book) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    book._id = bookIndex++;
+    booksFromFakeDB.push(book);
+    resolve({...book});
+  }, 500);
+});
+
+export const getBookByIdFromFakeXHR = (bookId) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const bookResponse = booksFromFakeDB.find(book => book._id === bookId);
+    if (bookResponse) resolve({...bookResponse});
+    else reject({status: 404, message: 'Book Not Found'});
+  }, 500);
+});
